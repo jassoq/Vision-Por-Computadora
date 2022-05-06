cda74d7db245c825a4c69613f300bad51826edcb
jassoq <104932844+jassoq@users.noreply.github.com>
2022-05-06 16:11:25 -0500

    Add files via upload

---
+++ Trampuesta.py
@@ -0,0 +1,14 @@
+
+#import pillow
+from PIL import Image
+
+img = Image.open('IMG.jpg')
+#print(img.mode)
+
+box = (150, 200, 600, 600)
+crop_img = img.crop(box) #left, upper, right, lower
+#crop_img.show()
+
+
+flip_img = img.transpose(Image.ROTATE_270)
+flip_img.show()
